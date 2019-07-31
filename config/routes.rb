9d9b57ab9a571ac929e190a29b2373d80aade730
JMeggesto <jackie.meggesto@gmail.com>
2019-07-30 22:56:14 -0400

    Stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  authenticate :user do
+    resources :stories, only: [:new, :create, :edit, :update, :destroy]
+  end
   root to: 'home#index'
 end
